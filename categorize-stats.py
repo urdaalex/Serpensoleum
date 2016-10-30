93d6433d19d0f67b1d571a993b6879d2d37bfc38
g3abdall <ma.absystems@gmail.com>
2016-10-29 22:29:26 -0400

    Ran accreddited folder, added statistics script for categorization results

---
+++ categorize-stats.py
@@ -0,0 +1,40 @@
+#Given a folder, it prints count of expected + actual results
+#Example Usage:   python scriptName.py DIR
+#Example Result:  Expected 54 Accredited; Got 5 Accredit, 6 False, 10 Unrelated
+
+import json
+import sys
+import os 
+
+FOLDER = "./" + sys.argv[1] + "/"
+cat_dictionary = {"a":0,"n":0,"f":0}
+
+def load_document(path):
+    with open(path) as json_data:
+        document = json.load(json_data)
+    return document
+
+def listdir_path(d):
+    return [os.path.join(d, f) for f in os.listdir(d)]
+
+def main():
+    true = None
+    group = None
+    counter = 0
+    for fn in listdir_path(FOLDER):
+	doc = load_document(fn)
+	cat_dictionary[doc["actual-search-type"]] = cat_dictionary[doc["actual-search-type"]] + 1
+	counter += 1
+	if true is None:
+	    true = doc["expected-search-type"]
+	    if true == "a":
+		group = "Accredited"
+	    elif true == "f":
+		group = "False"
+	    elif true == "n":
+		group = "Not Relevant"
+	 
+    print("Expected "+str(counter)+" "+group+"; Got "+str(cat_dictionary["a"])+" Accredited, "+str(cat_dictionary["f"])+" False, "+str(cat_dictionary["n"])+" Unrelated")
+
+if __name__ == "__main__":
+    main()
