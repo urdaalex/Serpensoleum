71a20f8f8ac47e69e542bead2a0624e38382da2c
gulara <paddze21@gmail.com>
2016-11-19 00:14:29 -0500

    Created chrome extension and added it to repo
    -To use the extension, make sure to have developer mode on in the
    chrome://extensions page.
    
    -Click "Load Unpacked Extension" and navigate to the extension directory.
    
    -Keep in mind that it isn't making use of actual classification results
    thus far, but it does make use of my parser so that should give you and
    idea of the delay to be expected.
    
    -Furthermore, I will clean up some of the code when I get a chance to. I
    focused heavily on making it possible to enable/disable the extension via
    the click of a button. This allows users to turn it off for when they're
    not performing medically related searches. It also allows them to clear
    the current classifications made on the current google page.

---
+++ extension/cs.js
@@ -0,0 +1,117 @@
+'use strict'
+
+// var port = chrome.runtime.connect({name: "enableDisable"});
+// port.postMessage({check: "Is enbaled?"});
+// port.onMessage.addListener(function(msg) {
+//   if (msg.response == false)
+//     // port.postMessage({answer: "Madame"});
+// 	removeAllScripts();
+//   else if (msg.question == "Madame who?")
+//     // port.postMessage({answer: "Madame... Bovary"});
+// 	injectAllScripts();
+// });
+
+// function onRequest(request) {
+//     if (request.action == 'start')
+//         // startExtension()
+//     	injectAllScripts();	
+//     else if (request.action == 'stop')
+//         // stopExtension()
+//     	removeAllScripts();
+//     sendResponse({});
+// }
+
+// chrome.extension.onMessage.addListener(onRequest);
+
+var cs_to_background_port = chrome.extension.connect({
+    name: "cs_to_background"
+});
+
+var background_to_cs = chrome.extension.connect({
+    name: "background_to_cs"
+});
+
+
+chrome.extension.onConnect.addListener(function(port) {
+    console.log("Connected .....");
+    if (port.name == "background_to_cs") {
+        port.onMessage.addListener(function(msg) {
+            console.log("message recieved" + msg);
+
+            if (msg.state == false) {
+                removeAllScripts();
+            } else if (msg.state == true) {
+                injectAllScripts();
+            }
+        });
+    }
+});
+
+function messageListener(request, sender, sendResponse) {
+	handleStateModification();
+
+	sendResponse({});
+
+    // sendResponse({ state: chrome.storage.sync.get('enabledStatus') });
+}
+
+chrome.runtime.onMessage.addListener(messageListener);
+
+// function manageResponse(msg) {
+//     console.log("message recieved" + msg);
+
+//     if (msg.state == false) {
+//         removeAllScripts();
+//     } else if (msg.state == true) {
+//         injectAllScripts();
+//     }
+// }
+
+function requestExtensionState() {
+    // cs_to_background_port.postMessage("get state");
+    // chrome.runtime.sendMessage("get state", manageResponse)
+
+    handleStateModification();
+}
+
+requestExtensionState();
+
+function handleStateModification() {
+    chrome.storage.sync.get('enabledStatus', function(obj) {
+        if (obj.enabledStatus == null) {
+            obj.enabledStatus = true;
+        }
+
+        if (obj.enabledStatus == true) {
+            injectAllScripts();
+        } else {
+            removeAllScripts();
+        }
+    });
+}
+
+function injectScript(file, node, id) {
+    var th = document.getElementsByTagName(node)[0];
+    var s = document.createElement('script');
+    s.setAttribute('id', id);
+    s.setAttribute('type', 'text/javascript');
+    s.setAttribute('src', file);
+    th.appendChild(s);
+}
+
+function removeAllScripts() {
+    $('#Z1Z1Z1Z1').remove();
+    $('#X1X1X1X1').remove();
+    $('.bs111check111bs').remove();
+}
+
+function injectAllScripts() {
+    injectScript(chrome.extension.getURL('jquery.js'), 'head', 'Z1Z1Z1Z1');
+    injectScript(chrome.extension.getURL('listener.js'), 'head', 'X1X1X1X1');
+}
+
+
+// var script = document.createElement('script');
+// script.textContent = actualCode;
+// (document.head||document.documentElement).appendChild(script);
+// script.parentNode.removeChild(script);
