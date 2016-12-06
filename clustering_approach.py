3baba41c6707c7b0df75e1a05d2b6a283061e32e
MunirAD <munir.al.dajani@mail.utoronto.ca>
2016-12-06 15:42:55 -0500

    fixed prediction making

--- clustering_approach.py
+++ clustering_approach.py
@@ -319,14 +319,11 @@ def main(argv):
             for i in range(len(test_sentences_tfisf)):
                 test_sentences_tfisf[i] = np.append(test_sentences_tfisf[i], [0] * (larger_length - len(test_sentences_tfisf[i])))
 
-        # Train a classifier on the sentences in the nearest documents
-        clf = SVC(kernel="poly", degree=3)
-        clf.fit(train_sentence_vectors, [nearest_sent_label_title[i][1] for i in range(len(nearest_sent_label_title))])
-
         # Now we can get nearest sentences for each sentence in test sentence
         for i in range(len(test_doc_sentences)):
             test_sentence = test_doc_sentences[i]
             test_sent_vector = test_sentences_tfisf[i]
+
             nearest_sentences_idxs = getNearestDocuments(test_sent_vector, train_sentence_vectors)
             '''
             print 'Test Sentence: ' + str(test_sentence)
@@ -334,6 +331,13 @@ def main(argv):
                 jth_nearest_sentence = nearest_sent_label_title[nearest_sentences_idxs[j]][0]
                 print 'Nearest sentences #' + str(j) + ': ' + str(jth_nearest_sentence)
             '''
+            # Train a classifier on the sentences in the cluster which test_sentence
+            # falls into
+            clf = SVC(kernel="poly", degree=3)
+            train_sentences = [train_sentence_vectors[nearest_sentences_idxs[i]] for i in range(len(nearest_sentences_idxs))]
+            train_sent_labels = [nearest_sent_label_title[nearest_sentences_idxs[j]][1] for j in range(len(nearest_sentences_idxs))]
+            assert len(train_sentences) == len(train_sent_labels)
+            clf.fit(train_sentences, train_sent_labels)
             predictions = []
 
             # predict the label for this sentence
