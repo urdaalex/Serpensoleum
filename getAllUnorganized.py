41de99ee692ecc06b32f8c17e114cf167360503a
MunirAD <munir.al.dajani@mail.utoronto.ca>
2016-12-17 02:37:38 -0500

    adding these files for the sake of having them as reference

--- getAllUnorganized.py
+++ getAllUnorganized.py
@@ -1,31 +1,37 @@
 import os
-from sys import exit
+import sys
 from shutil import copyfile
 
-data_path = 'data'
-new_dir = 'unorganized_data'
+def main(argv):
+    data_path = argv[0]
+    if (not os.path.exists(data_path)):
+        print data_path + ' does not exist!'
+        sys.exit(1)
 
-if os.path.exists(new_dir):
-    print new_dir + ' already exists'
-    exit(1)
+    new_dir = argv[1]
+    if os.path.exists(new_dir):
+        print new_dir + ' already exists'
+        sys.exit(1)
 
-os.mkdir(new_dir)
+    os.mkdir(new_dir)
+    for topic in os.listdir(data_path):
+        if topic == 'parsed':
+            continue
 
-for topic in os.listdir(data_path):
-    if topic == 'parsed':
-        continue
+        for category in os.listdir(os.path.join(data_path, topic)):
+            try:
+                x = category.split('-')[1]
+                if x == 'NOT CATEGORIZED':
+                    continue
+            except:
+                pass
 
-    for category in os.listdir(os.path.join(data_path, topic)):
-        try:
-            x = category.split('-')[1]
-            if x == 'NOT CATEGORIZED':
-                continue
-        except:
-            pass
+            for f in os.listdir(os.path.join(data_path, topic, category)):
+                if not f.endswith('.json'):
+                    continue
 
-        for f in os.listdir(os.path.join(data_path, topic, category)):
-            if not f.endswith('.json'):
-                continue
+                copyfile(os.path.join(data_path, topic, category, f),
+                        os.path.join(new_dir, f))
 
-            copyfile(os.path.join(data_path, topic, category, f),
-                    os.path.join(new_dir, f))
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- makeblackbox_clustering_approach.py
+++ makeblackbox_clustering_approach.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from html_parser import parse_document_tag_based
 
 def main(argv):
 
@@ -11,10 +10,10 @@ def main(argv):
     # The second arg is the percentage of the data which will be used as
     # training data
     proportion_train = (float(argv[1]))/100
-    proportion_test = 1 - percent_train
+    proportion_test = 1 - proportion_train
 
     # Get the full parsed data using the -tag method in html_parser.py
-    all_data_parsed = getParsed(data_path)
+    folder_with_parsed = getParsed(data_path)
 
 
 def getParsed(data_path):
@@ -22,7 +21,19 @@ def getParsed(data_path):
     Given the path to the folder containing all the data, this function
     parses all the data using the -tag method in html_parser.py
     '''
-    
+    parsed_data_name = 'parsed_' + data_path
+    unorg_folder_name = "unorganized_" + parsed_data_name
+
+    if os.path.exists(parsed_data_name):
+        if os.path.exists(unorg_folder_name):
+            print unorg_folder_name + " exists prior to execution"
+        else:
+            os.system("python getAllUnorganized.py " + parsed_data_name + " " + unorg_folder_name)
+        return unorg_folder_name
+
+    os.system("python html_parser.py -tag " + data_path + " " + parsed_data_name)
+    os.system("python getAllUnorganized.py " + parsed_data_name + " " + unorg_folder_name)
+    return unorg_folder_name
 
 if __name__ == "__main__":
     main(sys.argv[1:])
